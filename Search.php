82340d0edf13a7d9f633f78c60fb99e88e18aab9
DXManh <dxmanh92@gmail.com>
2014-12-23 14:47:40 +0700

    Create Search.php

---
+++ Search.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Mua sách trực tuyến</title>
+        <link rel="stylesheet" type="text/css" href="css/top_header.css"/>
+        <link rel="stylesheet" type="text/css" href="css/content.css"/>
+        <link rel="stylesheet" type="text/css" href="css/left_content.css"/>
+        <link rel="stylesheet" type="text/css" href="css/middle_content.css"/>
+        <link rel="stylesheet" type="text/css" href="css/footer.css"/>
+		<style type="text/css">
+			a:link{
+				text-decoration:none;
+				color:#05b2e9;
+			}
+			a:visited {
+				color: #05b2e9;
+			}
+			a:hover {
+				color: #3C3;
+			}
+			a:active {			
+				color: #F00;
+			}
+		</style>
+</head>
+<body>
+    <div class="top">
+    	<center><a href="index.html"><img src="images/File-1414989219.png" alt="" width="1140" height="65" /></a></center>
+	</div>
+    <!--------- START HEADER----------------->
+	<div class="header">
+        <div id="homepage_icon"><a href="index.html"><abbr title="Home Page"><img src="images/Homepage_icon.jpg" width="80" height="97"/></abbr></a></div>
+	  	<div class="search_area">
+            <form>
+            	<div class="search_div"><input type="text" name="search" class="search"/></div>
+            	<input name="button" type="button" class="button" value="TÌM" />
+            </form>
+                <div class="title"><a href="index.html"><img src="images/title.png" width="261" height="35" alt="muasachonline.vn" /></a></div>
+        </div>
+		<div class="login_area">
+           	<div class="login_icon"><img src="images/login_icon.png" width="45" height="41" align="middle" /></div>
+            <div class="login_text"><a href="sign_in.html">Đăng nhập</a> | <a href="registered.html">Đăng ký</a></div>
+            <div class="hotline"><img src="images/phone_icon.jpg" width="15" height="15" /><span class="hotline_text">Hotline:</span> <span class="phone_number">1900-6035</span><span style="font-size:12px">(8-21h kể cả T7,CN)</span></div>
+        </div>
+    </div>
+    <br />
+	<hr />
+    <!--------- END HEADER ------------------>
+    <div class="main">
+    	<!----- START CONTENT --------->
+        	<div class="start">
+            	<span><a href="index.html">Trang chủ</a> / <a href="sach_tieng_anh.html">Sách Tiếng Anh</a></span> / <strong>Mua sách trực tuyến Giảm Giá Hơn 20% - Nhà Sách HEDSPI</strong>
+            </div>
+            <!----- START left_content ------->
+            <div class="left_content">
+            	<div class="title_box1">DANH MỤC SẢN PHẨM</div>
+                <div class="left_menu1">
+                    <ul class="left_menu_content1">
+                        <li><a href="sach_tieng_anh.html">Sách Tiếng Anh</a></li>
+                        <li><a href="sach_vanhoc-tieuthuyet.html">Sách Văn Học - Tiểu Thuyết</a></li>
+                        <li><a href="#">Sách Kinh Tế</a></li>
+                        <li><a href="#">Sách Chuyên Ngành</a></li>
+                        <li><a href="#">Sách Kỹ Năng Sống - Nghệ Thuật Sống</a></li>
+                        <li><a href="#">Sách Giáo Khoa - Tham Khảo</a></li>
+                        <li><a href="#">Sách Học Ngoại Ngữ - Từ Điển</a></li>
+                        <li><a href="#">Sách Cho Tuổi Mới Lớn</a></li>
+                        <li><a href="#">Sách Truyện Thiếu Nhi</a></li>
+                        <li><a href="#">Truyện Tranh, Manga, Comic</a></li>
+                        <li><a href="#">Sách Văn Hoá - Nghệ Thuật - Du Lịch</a></li>
+                        <li><a href="#">Tạp Chí</a></li>
+                        <li style="border: none;"><a href="#">Sách Nuôi Dạy Con</a></li>
+                    </ul>
+                </div>               
+                <div class="title_box2">TOP BESTSELLER</div>
+                <div class="left_menu2">
+                    <ul class="left_menu_content2">
+                        <li>
+                        	<div class="book">                            	
+                                <div class="book_icon"><a href="#"><img src="images/b_a-s_ch-ngo_i.png" width="70" height="98" /></a></div>
+                                <div class="book_title"><a href="#">Cà phê cùng Tony</a></div>
+                                <div class="book_note">Tony buổi sáng</div>
+                                <div class="book_note">Bìa Mềm</div>
+                                <div class="book_sale">-31%</div>
+                                <div class="book_cost">50.000 ₫</div>
+                        	</div>
+                        </li>
+                        <li>
+                        	<div class="book">                            	
+                                <div class="book_icon"><a href="#"><img src="images/yeu-di-thoi-muon-lam-roi.jpg" width="70" height="98" /></a></div>
+                                <div class="book_title"><a href="#"><abbr title="Tập Thơ: Yêu Đi Thôi,Muộn lắm rồi">Tập Thơ: Yêu Đi Thôi,...</abbr></a></div>
+                                <div class="book_note">Nhiều Tác Giả</div>
+                                <div class="book_note">Bìa Mềm</div>
+                                <div class="book_sale">-30%</div>
+                                <div class="book_cost">42.000 ₫</div>
+                        	</div>
+                        </li>
+                        <li>
+                        	<div class="book">                            	
+                                <div class="book_icon"><a href="#"><img src="images/hoge.jpg" width="70" height="98" /></a></div>
+                                <div class="book_title"><a href="#"><abbr title="Luyện Nói Tiếng Anh Như Người Bản Địa">Luyện Nói Tiếng Anh Như ...</abbr></a></div>
+                                <div class="book_note">A.J.Hoge</div>
+                                <div class="book_note">Bìa Mềm</div>
+                                <div class="book_sale">-24%</div>
+                                <div class="book_cost">68.000 ₫</div>
+                        	</div>
+                        </li>
+                        <li>
+                        	<div class="book">                            	
+                                <div class="book_icon"><a href="#"><img src="images/nhat-ky-meo-ngoc.jpg" width="70" height="98" /></a></div>
+                                <div class="book_title"><a href="#"><abbr title="Nhật Ký Mèo Ngốc">Nhật Ký Mèo Ngốc</abbr></a></div>
+                                <div class="book_note">Mèo Mốc</div>
+                                <div class="book_note">Bìa Mềm</div>
+                                <div class="book_sale">-30%</div>
+                                <div class="book_cost">55.000 ₫</div>
+                        	</div>
+                        </li>
+                        <li>
+                        	<div class="book">                            	
+                                <div class="book_icon"><a href="#"><img src="images/bia_ai_roi_cung_khac_1.jpg" width="70" height="98" /></a></div>
+                                <div class="book_title"><a href="#"><abbr title="Ai Rồi Cũng Khác">Ai Rồi Cũng Khác</abbr></a></div>
+                                <div class="book_note">Hamlet Trương - Iris Cao</div>
+                                <div class="book_note">Bìa Mềm</div>
+                                <div class="book_sale">-25%</div>
+                                <div class="book_cost">52.000 ₫</div>
+                        	</div>
+                        </li>
+                        <li>
+                        	<div class="book">                            	
+                                <div class="book_icon"><a href="#"><img src="images/chuyen-con-meo-day-hai-au-bay.jpg" width="70" height="98" /></a></div>
+                                <div class="book_title"><a href="#"><abbr title="Chuyện Con Mèo Dạy Hải Âu Bay">Chuyện Con Mèo Dạy Hải ...</abbr></a></div>
+                                <div class="book_note">Luis Sepulveda</div>
+                                <div class="book_note">Bìa Mềm</div>
+                                <div class="book_sale">-26%</div>
+                                <div class="book_cost">26.000 ₫</div>
+                        	</div>
+                        </li>
+                        <li>
+                        	<div class="book">                            	
+                                <div class="book_icon"><a href="#"><img src="images/bookcare-40.png" width="70" height="98" /></a></div>
+                                <div class="book_title"><a href="#"><abbr title="Dịch vụ BookCare 40 quyển">Dịch vụ BookCare 40 quyển</abbr></a></div>
+                                <div class="book_note"></div>
+                                <div class="book_note">Bìa Mềm</div>
+                                <div class="book_sale"></div>
+                                <div class="book_cost">99.000 ₫</div>
+                        	</div>
+                        </li>
+                        <li>
+                        	<div class="book">                            	
+                                <div class="book_icon"><a href="#"><img src="images/o-day-bay-gio-hanh-phuc.jpg" width="70" height="98" /></a></div>
+                                <div class="book_title"><a href="#"><abbr title="Ở Đây, Bây Giờ Và Hạnh Phúc">Ở Đây, Bây Giờ Và ...</abbr></a></div>
+                                <div class="book_note">Nhiều Tác Giả</div>
+                                <div class="book_note">Bìa Mềm</div>
+                                <div class="book_sale">-20%</div>
+                                <div class="book_cost">44.000 ₫</div>
+                        	</div>
+                        </li>
+                        <li style="border: none;">
+                        	<div class="book">                            	
+                                <div class="book_icon"><a href="#"><img src="images/ben-nhau-tron-doi-.jpg" width="70" height="98" /></a></div>
+                                <div class="book_title"><a href="#"><abbr title="Bên Nhau Trọn Đời-Tái Bản 2014(Tặng kèm poster)">Bên Nhau Trọn Đời...</abbr></a></div>
+                                <div class="book_note">Cố Mạn</div>
+                                <div class="book_note">Bìa Mềm</div>
+                                <div class="book_sale">-30%</div>
+                                <div class="book_cost">62.000 ₫</div>
+                        	</div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="title_box3">
+                	<img src="images/Homepage/Untitled.png" width="221" height="365" />
+                </div>
+            </div>
+            <!------- END left_content ------->
+            
+			<?php
+				$host = "localhost";
+				$user = "root";
+	
+				$db="data";										// CSDL sao chép vào đây
+				$search = $_POST['search'];  					// keysearch nhập vào ở index
+	
+				$conn = mysql_connect($host, $user,"","bookstore") or die("Không truy cập đc.");
+				mysql_select_db ($db, $conn);					// Sao chép CSDL
+	
+				$sql = "SELECT * FROM books".$search;
+	
+				$query = mysql_query($sql) or die (mysql_error() );
+	
+				if(mysql_num_row($query) == 0){
+					echo("Không phù hợp.");
+					}
+				else{
+					while($row = mysql_fetch_array($query)){
+						echo $row['image']."<br>";
+						echo $row['title']."<br>";
+						echo $row['author']."<br>";
+						echo $row['price']."<br>";
+						echo $row['sale_off']."<br>";
+						}
+					}
+				mysql_close($conn);
+			?>
+			
+			
+			
+            <!------- START Middle_content ------->
+          <div class="middle_content">
+          		<!----------SÁCH Tìm Thấy-------------->
+            	
+				<?php
+          $host = "localhost";
+	        $user = "root";
+	        $password = "123456";
+	
+	        $db="data";										// CSDL
+  	      $search = $_POST['search'];  					// keysearch nhập vào ở index
+	
+	        $conn = mysql_connect($host, $user, $password) or die("Không truy cập đc.");
+	        mysql_select_db ($db, $conn);
+	
+	        $sql = "select * form".$search;
+	
+	        $query = mysql_query($sql) or die (mysql_error() );
+	
+	        if(mysql_num_row($query) == 0){
+		        echo("Không tìm thấy.");
+	          }
+	        else{
+		        while($row = mysql_fetch_array($query)){
+		          echo "<td>";
+		          echo "<tr>".$row['image']."</tr>";
+		          echo "<tr>".$row['title']."</tr>";
+		          echo "<tr>".$row['author']."</tr>";
+		          echo "<tr>".$row['price']."</tr>";
+		          echo "<tr>".$row['sale_off']."</tr>";
+		          echo "</td>";
+		          }
+        	  }
+	        mysql_close($conn);
+        ?>
+				
+            <!------- END Middle_content ------->
+        </div>
+	</div>
+        <div class="clr"></div>
+        <!-------------- FOOTER ----------------->
+		<div class="footer">
+       		<div class="leter_icon"><img src="images/Homepage/icon_sign.png" width="25" height="25" /></div>
+			<span id="footer_title">Đăng Kí Nhận Bản Tin Hedspi - <span style="font-weight:normal">Nhiều Tiện Lợi và Ưu Đãi đang chờ bạn!</span></span>
+        	<div class="hand_icon"><img src="images/Homepage/hand-right-128.png" width="40" height="40" /></div>
+        	<form>
+            	<div class="input"><input type="text" name="input" class="input_class"/></div>
+            	<input name="button" type="button" class="button_class" value="Đăng Ký - Nhận Tiện Ích" />
+            </form>
+             <div class="clr"></div>
+            <p>© Đại Học Bách Khoa Hà Nội<br /><br />Lớp Thực Hành Cơ Sở Dữ Liệu - Việt Nhật B- K57<br />Thành viên: Ngô Văn Thịnh, Trần Bảo Huy, Đặng Mỹ Linh, Đồng Xuân Mạnh</p>
+        </div>
+</body>
+</html>
