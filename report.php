04d3897fd7d4fca784d7c04731a1889115f01a23
linh-dang <mlinh211@gmail.com>
2014-12-23 18:55:28 +0700

    Báo lỗi

---
+++ report.php
@@ -0,0 +1,107 @@
+<!doctype html>
+<html lang=''>
+<head>
+<meta charset='utf-8'>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/report.css">
+<link rel="stylesheet" href="css/footer.css">
+<link rel="stylesheet" href="css/top_header.css">
+<link rel="stylesheet" href="css/cus-account.css">
+
+<script src="script.js"></script>
+<title>Báo lỗi</title>
+</head>
+<body>
+	<div class="top">
+    	<center><a href="index.html"><img src="images/File-1414989219.png" alt="" width="1140" height="65"></a></center>
+	</div>
+	<div class="header">
+        <div id="homepage_icon"><a href="index.html"><abbr title="Home Page"><img src="images/Homepage_icon.jpg" width="80" height="97"/></abbr></a></div>
+	  	<div class="search_area">
+            <form>
+            	<div class="search_div"><input type="text" name="search" class="search"/></div>
+            	<input name="button" type="button" class="button" value="TÌM" />
+            </form>
+                <div class="title"><a href="index.html"><img src="images/title.png" width="261" height="35" alt="muasachonline.vn" /></a></div>
+        </div>
+		<div class="login_area">
+           	<div class="login_icon"><img src="images/login_icon.png" width="45" height="41" align="middle" /></div>
+            <div class="login_text"><a href="sign_in.html">Đăng nhập</a> | <a href="registered.html">Đăng ký</a></div>
+            <div class="hotline"><img src="images/phone_icon.jpg" width="15" height="15" /><span class="hotline_text">Hotline:</span> <span class="phone_number">1900-6035</span><span style="font-size:12px">(8-21h kể cả T7,CN)</span></div>
+        </div>
+    </div>
+    <br />
+	<hr>
+<div class="container">
+	
+	<div class="clearfix">
+		<div class="layout">
+			<div class="col-left">
+				<div class="avatar clearfix">
+					<h3>Điều hành trang web</h3>
+					<div class="all-left">
+						<a href="#" title="User-name">
+						<img src="images/login_icon.png" alt="User-name" width="45" height="45">
+						</a>
+						<a href="#" title="đặng linh">
+							<span class="u-name">đặng linh	</span>		</a>
+					</div>
+				</div>
+				<ul>
+					<li><a href="ad-account.php">Thông tin chung tài khoản</a></li>
+					<li><a href="upload-new-book.php">Thêm sách mới</a></li>
+					<li><a href="store.php">Kho hàng</a></li>
+					<li><a href="sales-off.php">Quản lý khuyến mại</a></li>
+					<li><a href="customers.php">Quản lý khách hàng</a></li>
+					<li><a href="ad-order-history.php">Quản lý đơn hàng</a></li>
+					
+					<li  class="on">Báo lỗi<span style="color: red;font-size: 1.1em;"> <b>!</b></span></li>
+				</ul>
+			</div>
+			<div class="col-main">
+				<div class="main-1">
+				<h1>Báo lỗi</h1>
+				</div>
+				<div class="main-2">
+				<p><strong>Xin chào, user!</strong><br>
+				Nếu phát hiện lỗi trong trang web hãy báo ngay cho chúng tôi.</p>
+				</div>
+				<div class="report-problem">
+					<form id=""  method="get">
+					<div class="clearfix">
+						<label for="report">
+								
+						</label> </br>
+						<textarea  id="report" name="report" title="Report Problem" maxlength="200"></textarea>
+					</div>
+						<button type="button" class="signin-button" onclick="window.location='#';">
+							Báo lỗi</button>
+					</form>
+				</div>
+				<div class="button-set">
+					<a href="index.php" class="left">« Trở lại trang chủ</a>
+				</div>
+				
+			</div>
+			
+		</div>
+	</div>
+
+</div>	
+<div class="footer">
+       		<div class="leter_icon"><img src="images/Homepage/icon_sign.png" width="25" height="25"></div>
+			<span id="footer_title">Đăng Kí Nhận Bản Tin Hedspi - <span style="font-weight:normal">Nhiều Tiện Lợi và Ưu Đãi đang chờ bạn!</span></span>
+        	<div class="hand_icon"><img src="images/Homepage/hand-right-128.png" width="40" height="40"></div>
+        	<form>
+            	<div class="input"><input type="text" name="input" class="input_class"></div>
+            	<input name="button" type="button" class="button_class" value="Đăng Ký - Nhận Tiện Ích">
+            </form>
+             <div class="clr"></div>
+            <p>© Đại Học Bách Khoa Hà Nội<br><br>Lớp Thực Hành Cơ Sở Dữ Liệu - Việt Nhật B- K57<br>Thành viên: Ngô Văn Thịnh, Trần Bảo Huy, Đặng Mỹ Linh, Đồng Xuân Mạnh</p>
+        </div>
+
+
+
+</body>
+<html>
