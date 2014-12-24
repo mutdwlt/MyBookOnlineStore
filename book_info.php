f049ed7277fbd5ecaf315aa90641823384905c9c
mutdwlt <mutdwlt@gmail.com>
2014-12-25 00:42:43 +0700

    Update book_info.php

--- book_info.php
+++ book_info.php
@@ -31,7 +31,6 @@
         <link rel="stylesheet" type="text/css" href="css/content.css"/>
         <link rel="stylesheet" type="text/css" href="css/book_info.css"/>
         <link rel="stylesheet" type="text/css" href="css/footer.css"/>
-		<link rel="stylesheet" href="css/upload_book.css">
 		<style type="text/css">
 			a:link{
 				text-decoration:none;
@@ -56,28 +55,39 @@
 	<div class="header">
         <div id="homepage_icon"><a href="index.php"><abbr title="Home Page"><img src="images/Homepage_icon.jpg" width="80" height="97"/></abbr></a></div>
 	  	<div class="search_area">
-            <form>
+            <form action="search_result.php" method="post">
             	<div class="search_div"><input type="text" name="search" class="search"/></div>
-            	<input name="button" type="button" class="button" value="TÌM" />
+            	<input name="submit" type="submit" class="button" value="TÌM" />
             </form>
-                <div class="title"><a href="index.php"><img src="images/title.png" width="261" height="35" alt="muasachonline.vn" /></a></div>
+                <div class="title"><a href="search.php"><img src="images/title.png" width="261" height="35" alt="muasachonline.vn" /></a></div>
         </div>
 		<div class="login_area">
            	<div class="login_icon"><img src="images/<?php if(isset($_SESSION['role'])){if($_SESSION['role']==1) echo "admin_icon.png"; else echo"login_icon.png";}else echo"login_icon.png";?>" width="45" height="41" align="middle" /></div>
 			<?php
 				if(isset($_SESSION['name']))
 				{
-				?>
-				<div class="login_text"><a href="<?php if($_SESSION['role']==1) echo'ad_account.php'; else echo 'cus_account.php';?>">Xin chào <?php echo $_SESSION["name"];?></a> | <a href="logout.php">Đăng Xuất</a></div>
-				<?php
+					if($_SESSION['role']==1)
+					{
+					?>
+						<div class="login_text">Chào Admin</div>
+						<div class="login_text"><a href="ad_account.php">Trang quản lý</a> | <a href="logout.php">Đăng Xuất</a></div>
+					<?php					
+					}
+					else
+					{
+					?>
+						<div class="login_text"> Chào <?php echo $_SESSION['name'];?> - <a href="logout.php">Log out</a></div>
+						<div class="login_text"><a href="cus_account.php">Trang cá nhân</a> | <a href="cus_cart.php">Giỏ hàng</a></div>
+					<?php
+					}
 				}
 				else
 				{
 				?>
-				<div class="login_text"><a href="login.php">Đăng nhập</a> | <a href="registered.php">Đăng ký</a></div>
+				<div class="login_text1"><a href="login.php">Đăng nhập</a> | <a href="registered.php">Đăng ký</a></div>
 				<?php
 				}
-			?>  
+			?> 
             <div class="hotline"><img src="images/phone_icon.jpg" width="15" height="15" /><span class="hotline_text">Hotline:</span> <span class="phone_number">1900-6035</span><span style="font-size:12px">(8-21h kể cả T7,CN)</span></div>
         </div>
     </div>
@@ -108,6 +118,8 @@
 				<p>Tiết kiệm: <strong><?php echo $price-$gia_ban;?>.000 ₫</strong></p>
             </div>
 			<?php
+			if(isset($_SESSION['role']))
+			{
 			if($_SESSION['role']==0)
 			{
 			?>
@@ -126,6 +138,17 @@
 					<div class="button-set">
 							<input type="submit" name="chinhsua" class="signin-button" value="Chỉnh sửa">
 					</div>
+                </form>
+				</div>
+			<?php
+			}
+			}
+			else{
+			?>
+				<div class="dat_hang">		
+            	<form action="addcart.php">
+					<input type="hidden" name="item" value="<?php echo $data['book_ID']; ?>">
+					<input type="image" src="images/<?php if(strtotime($publish_date)>strtotime($today)) echo"pre-order.png"; else echo "order.png"; ?>">
                 </form>
 				</div>
 			<?php
